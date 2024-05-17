087eadae06f105953f145d1f85ad2f8012b05869
Deshan Maduranga <deshanm123@gmail.com>
2024-05-17 15:27:36 +0100

    HttpPost / posting/submiting items to cart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { CatalogComponent } from './catalog/catalog.component';
+ import { CatalogComponent } from './catalog/catalog.component';
 import {SiteHeaderComponent} from './site-header/site-header.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
+import { HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -15,9 +16,7 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
     SiteHeaderComponent,
     ProductDetailsComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule , HttpClientModule ],
   providers: [],
   bootstrap: [AppComponent]
 })
