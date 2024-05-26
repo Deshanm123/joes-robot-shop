415cb7f304a9db8e5e0a12776a2180d0dd06057d
Deshan Maduranga <deshanm123@gmail.com>
2024-05-26 14:27:52 +0100

    signed in validations and nav bar

--- src/app/site-header/site-header.component.ts
+++ src/app/site-header/site-header.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IUser } from '../user/user.model';
+import { UserService } from '../user/user.service';
 
 @Component({
   selector: 'bot-site-header',
   templateUrl: './site-header.component.html',
   styleUrls: ['./site-header.component.css']
 })
-export class SiteHeaderComponent {
 
-  constructor() { }
+export class SiteHeaderComponent implements OnInit {
 
+  user : IUser| null = null;
+  displaySignOutMenu  : boolean = false;
+
+  constructor(private userService : UserService) { }
+ 
+  ngOnInit(){
+    this.userService.getUser().subscribe({
+      next : (user) =>{this.user = user}
+    })
+  }
+  
+  toggleSignOutMenu(){
+    this.displaySignOutMenu = !this.displaySignOutMenu;
+  }
+
+  signOut(){
+    this.userService.signOut();
+    this.displaySignOutMenu = false;
+  }
+  
 }
--- src/app/user/sign-in/sign-in.component.ts
+++ src/app/user/sign-in/sign-in.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IUserCredentials } from '../user.model';
+import { UserService } from '../user.service';
+import { Router } from '@angular/router';
  
 @Component({
   selector: 'app-sign-in',
@@ -9,8 +11,21 @@ import { IUserCredentials } from '../user.model';
 
 export class SignInComponent {
    credentials: IUserCredentials = {email : '' , password : ''}
+   signInError = false;
 
-   constructor(){
+   constructor(private userService : UserService, private router : Router ){
 
+   }
+
+   signInUserFun(){
+    //reset the flag every time clicked
+    this.signInError = false;
+
+    //since sign in return an observable we can subscribe and use the data
+    this.userService.signIn(this.credentials)
+                    .subscribe({
+                       next: ()=> this.router.navigate(['/catalog']), //You pass in an array of URL segments to Router.navigate.
+                       error: ()=>this.signInError = true
+                    });
    }
 }
